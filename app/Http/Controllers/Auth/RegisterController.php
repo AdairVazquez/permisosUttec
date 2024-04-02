d712d9be4dd71df5caddcda3a2823c03e0223c06
Alan Adair Vazquez Cruz <adairvazquez@MacBook-Pro-de-Alan.local>
2024-04-02 10:48:46 -0600

    actualizacion

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -67,6 +67,8 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
+            'imagen' => 'https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png',
+            'id_google' => '0',
         ]);
     }
 }
